21fbda79515050398f872c4634c335f8a7937f28
Denis Burlakov <denis055023@gamil.com>
2023-03-02 09:05:03 +0100

    test

---
+++ 1.5/1.5.1.py
@@ -0,0 +1,28 @@
+# a, b = input().split()
+# print(f'{a} {b}')
+# a, b, c = map(int, input().split())
+# print(f'{a} {b} {c}')
+
+year = int(input())
+print(year + 1)
+
+num = int(input())
+print(num * 2)
+print(num / 2)
+
+lenght = float(input())
+print(lenght ** 2)
+
+a, b = int(input()), int(input()),
+print(a + b)
+
+a, b = float(input()), float(input())
+s = a * b
+p = 2 * (a + b)
+print(s, p)
+
+f = float(input())
+c = (f - 32) * 5 // 9
+print(c)
+a, b = int(input()), int(input())
+print(abs(a) + abs(b))
