8b7d2ab215ab9b61647e508affc416f1b35993c2
Denis Burlakov <denis055023@gmail.com>
2023-04-25 18:17:18 +0200

    start 5.7.1

--- Module_5/5.7/5.7.1.py
+++ Module_5/5.7/5.7.1.py
@@ -1,2 +1,12 @@
-for i in range(2):
-    print(i)
+n = int(input)
+a = []
+s = 0
+for i in range(n):
+    b = []
+    b = list(map(int, input().split))
+    a.append(b)
+for i in range(n):
+    for j in range(n):
+        if i == j:
+            s += a[i][j]
+print(s)
