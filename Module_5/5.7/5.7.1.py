77e7020ab9e8ad94cbad98fc684bfdae46b11f32
Denis Burlakov <denis055023@gmail.com>
2023-04-27 06:40:16 +0200

    start 5.8

--- Module_5/5.7/5.7.1.py
+++ Module_5/5.7/5.7.1.py
@@ -1,9 +1,9 @@
-n = int(input)
+n = int(input())
 a = []
 s = 0
 for i in range(n):
     b = []
-    b = list(map(int, input().split))
+    b = list(map(int, input().split()))
     a.append(b)
 for i in range(n):
     for j in range(n):
