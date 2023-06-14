6e33958d67650fa1dbd887bd7b81dff9b6020d6d
Denis Burlakov <denis055023@gmail.com>
2023-06-14 21:54:51 +0200

    7.3.3

---
+++ Module_5/5.8/5.8.2.py
@@ -0,0 +1,16 @@
+n = int(input())
+a, b, c = map(int, input().split())
+
+matrix = [[0] * n for _ in range(n)]
+
+for i in range(n):
+    for j in range(n):
+        if i < j:
+            matrix[i][j] = a
+        elif i > j:
+            matrix[i][j] = b
+        else:
+            matrix[i][j] = c
+
+for row in matrix:
+    print(' '.join(map(str, row)))
---
+++ Module_7/7.3/7.3.2.py
@@ -0,0 +1,2 @@
+def is_person_teenager(year):
+    return True if 12 <= year <= 17 else False
